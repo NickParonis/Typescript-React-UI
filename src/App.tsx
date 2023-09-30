4ce5e0accbd7e5df2a43c35aef1208096b243dd6
unknown <nikolaosparonis@gmail.com>
2023-09-30 13:52:23 +0300

    initialize

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import Stars from './components/Stars';
+
+function App(){
+  return <div><Stars /></div>;
+};
+
+export default App;
---
+++ src/components/Stars.tsx
@@ -0,0 +1,18 @@
+function Stars(){
+
+    const stars=[
+        'stars',
+        'stars2'
+    ];
+
+    return (
+        <>
+            <div className="bg-animation">
+                {stars.map( (item) => (
+                    <div key={item} id={item}></div>
+                ))};
+            </div>
+        </>
+    );
+}
+export default Stars;
